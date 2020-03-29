926f9ce51edfb9d485862f8b34a28292ef76330a
Maklero <michalgolda@vp.pl>
2020-03-29 15:48:02 +0200

    Add react-router-dom module

--- app/App.js
+++ app/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { MainView } from './views';
-import { LoadingSpinComponent } from './components';
+import { MainView, LoginView } from './views';
 
 export default class App extends Component {
   render() {
     return (
-      <LoadingSpinComponent/>
+      <Router>
+        <Switch>
+          <Route path="/login" component={LoginView}/>
+          <Route path="/" component={MainView}/>
+        </Switch>
+      </Router>
     );
   }
 }
