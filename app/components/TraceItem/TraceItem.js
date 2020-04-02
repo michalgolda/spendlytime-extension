1cd92f5bc4d9d82831daeb2698afe070441288d3
Maklero <michalgolda@vp.pl>
2020-04-02 15:53:29 +0200

    Refactor class component to function

--- app/components/TraceItem/TraceItem.js
+++ app/components/TraceItem/TraceItem.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import style from './TraceItem.css';
 
-export default class TraceItem extends Component{
-    render(){
+function TraceItem(props){
+    const { url, time} = props;
 
-        const { url, time} = this.props;
-
-        return(
-            <div className={style.trace}>
-                <div className={style.trace__url}>
-                    <p className={style.trace__url__text}>{url}</p>
+    return(
+        <div className={style.trace}>
+            <div className={style.trace__url}>
+                <p className={style.trace__url__text}>{url}</p>
+            </div>
+            <div className={style.trace__details}>
+                <div className={style.trace__details__timer}>
+                    <p className={style.trace__details__timer__value}>{time}</p>
                 </div>
-                <div className={style.trace__details}>
-                    <div className={style.trace__details__timer}>
-                        <p className={style.trace__details__timer__value}>{time}</p>
-                    </div>
-                    <div className={style.trace__details__action}>
-                        <p className={style.trace__details__action__text}>•••</p>
-                    </div>
+                <div className={style.trace__details__action}>
+                    <p className={style.trace__details__action__text}>•••</p>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+TraceItem.propTypes = {
+    url: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired
+}
+
+export default TraceItem;
