53cfd9bbafce271a9496d8231d829f0bdbfcd380
Maklero <michalgolda@vp.pl>
2020-04-02 15:59:15 +0200

    Refactor class component to func

--- app/components/LoginForm/LoginForm.js
+++ app/components/LoginForm/LoginForm.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import style from './LoginForm.css';
 
-export default class LoginForm extends Component{
-    render(){
-        return(
-            <form className={style.login}>
-                <div className={style.login__group}>
-                    <input
-                        className={`${style.login__input} ${style.login__input_email}`}
-                        type="text"
-                        placeholder="Email"
-                    />
-                </div>
+export default function LoginForm(){
+    return(
+        <form className={style.login}>
+            <div className={style.login__group}>
+                <input
+                    className={`${style.login__input} ${style.login__input_email}`}
+                    type="text"
+                    placeholder="Email"
+                />
+            </div>
 
-                <div className={style.login__group}>
-                    <input
-                        className={`${style.login__input} ${style.login__input_password}`}
-                        type="password"
-                        placeholder="Hasło"
-                    />
-                </div>
-                <button className={style.login__button}>Zaloguj się</button>
-            </form>
-        );
-    }
+            <div className={style.login__group}>
+                <input
+                    className={`${style.login__input} ${style.login__input_password}`}
+                    type="password"
+                    placeholder="Hasło"
+                />
+            </div>
+            <button className={style.login__button}>Zaloguj się</button>
+        </form>
+    );
 }
