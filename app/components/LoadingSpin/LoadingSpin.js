c51c802ef9a3447eb77f99249da7b8503785decc
Maklero <michalgolda@vp.pl>
2020-04-02 15:58:06 +0200

    Refactor from class component to func

--- app/components/LoadingSpin/LoadingSpin.js
+++ app/components/LoadingSpin/LoadingSpin.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import style from './LoadingSpin.css';
 
-export default class LoadingSpin extends Component{
-    render(){
-        return(
-            <div className={style.spinner}>
-                <div className={style.rect1}></div>
-                <div className={style.rect2}></div>
-                <div className={style.rect3}></div>
-                <div className={style.rect4}></div>
-                <div className={style.rect5}></div>
-            </div>
-        );
-    }
+
+export default function LoadingSpin(){
+    return(
+        <div className={style.spinner}>
+            <div className={style.rect1}></div>
+            <div className={style.rect2}></div>
+            <div className={style.rect3}></div>
+            <div className={style.rect4}></div>
+            <div className={style.rect5}></div>
+        </div>
+    );
 }
