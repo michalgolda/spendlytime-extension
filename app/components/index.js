8bfa66a01366db00041b5580d5319203f0ac99cf
Maklero <michalgolda@vp.pl>
2020-04-03 18:01:44 +0200

    Remove LoginForm component

--- app/components/index.js
+++ app/components/index.js
@@ -1,13 +1,11 @@
 import Navbar from './Navbar/Navbar';
 import WithAuth from './WithAuth/WithAuth';
 import TraceItem from './TraceItem/TraceItem';
-import LoginForm from './LoginForm/LoginForm';
 import LoadingSpin from './LoadingSpin/LoadingSpin';
 
 export {
     Navbar,
     WithAuth,
     TraceItem,
-    LoginForm,
     LoadingSpin
 }
