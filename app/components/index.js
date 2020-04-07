252f25ac6f4de67fc5ee7f4fad98ca214a941e20
Michał Gołda <michalgolda@vp.pl>
2020-04-07 19:36:33 +0200

    Delete withAuth component

--- app/components/index.js
+++ app/components/index.js
@@ -1,11 +1,9 @@
 import Navbar from './Navbar/Navbar';
-import WithAuth from './WithAuth/WithAuth';
 import TraceItem from './TraceItem/TraceItem';
 import LoadingSpin from './LoadingSpin/LoadingSpin';
 
 export {
     Navbar,
-    WithAuth,
     TraceItem,
     LoadingSpin
 }
