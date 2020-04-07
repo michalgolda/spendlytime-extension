e2898eff2bec6b79e7fb646336a423d58b46debe
Michał Gołda <michalgolda@vp.pl>
2020-04-07 20:47:10 +0200

    Fix "Cannot create item with duplicate id..." on create contextMenu

--- chrome/extension/background/contextMenus.js
+++ chrome/extension/background/contextMenus.js
@@ -1,13 +1,22 @@
 const CONTEXT_MENU_ID = "spendlytime-context-menu";
 
-chrome.contextMenus.create({
-    id: CONTEXT_MENU_ID,
-    title: "Start track timer",
-    contexts: ["all"]
+/**
+ * Remove function is required execute because catch runtime.LastError,
+ * "Cannot create item with duplicate id..." this function on load extension
+ * delete exist context menu and create new in callback func.
+ */
+chrome.contextMenus.removeAll(function () {
+    chrome.contextMenus.create({
+        id: CONTEXT_MENU_ID,
+        title: "Start timer",
+        contexts: ["all"],
+    });
 });
 
 chrome.contextMenus.onClicked.addListener((event) => {
-    if(event.menuItemId === CONTEXT_MENU_ID){
+    if (event.menuItemId === CONTEXT_MENU_ID) {
         console.log("Clicked on context menu option.");
     }
 });
+
+//  Cannot create item with duplicate id
