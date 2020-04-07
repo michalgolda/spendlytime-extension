710542b00fca02dc9c512f202473e0a7a8d0a1a0
Michał Gołda <michalgolda@vp.pl>
2020-04-07 20:09:08 +0200

    Add handling ADD_TRACE_SUCCESS trace support update state

--- app/reducers/traces.reducer.js
+++ app/reducers/traces.reducer.js
@@ -1,18 +1,26 @@
-import { tracesConstants } from '../constants';
+import { tracesConstants } from "../constants";
 
 const initialState = { loading: false, data: [] };
-
-export function tracesReducer(state = initialState, action){
-    switch(action.type){
+export function tracesReducer(state = initialState, action) {
+    switch (action.type) {
         case tracesConstants.FETCH_TRACES_REQUEST:
             return { loading: true };
         case tracesConstants.FETCH_TRACES_SUCCESS:
             return {
                 loading: false,
-                data: action.data
-            }
+                data: action.data,
+            };
         case tracesConstants.FETCH_TRACES_ERROR:
             return { loading: true };
+        case tracesConstants.ADD_TRACE_SUCCESS:
+            return {
+                loading: false,
+                data: [
+                    ...state.data, {
+                        ...action.data
+                    }
+                ]
+            };
         default:
             return state;
     }
