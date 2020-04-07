1f97932d0d0fe98f822fe097eb060d073ae7017b
Michał Gołda <michalgolda@vp.pl>
2020-04-07 20:19:42 +0200

    Fix error connected witch storage is not set a current tab data

--- chrome/extension/background/tabs.js
+++ chrome/extension/background/tabs.js
@@ -1,16 +1,8 @@
+/**
+ * This event listener is get active tab for user browser and set to local storage,
+ * storage is forwarded to redux initital state.
+ */
 chrome.tabs.onActivated.addListener(function(){
-    chrome.tabs.query({ active: true, currentWindow: true}, function(tabs){
-        chrome.storage.local.set({
-            state: {
-                browserData: {
-                    currentTab: tabs[0]
-                }
-            }
-        });
-    });
-});
-
-chrome.runtime.onStartup.addListener(function(){
     chrome.tabs.query({ active: true, currentWindow: true}, function(tabs){
         chrome.storage.local.set({
             state: {
