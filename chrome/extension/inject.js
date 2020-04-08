2597a8e8a8bf60d2f9afda0142990c1b41b0a3dd
Michał Gołda <michalgolda@vp.pl>
2020-04-08 21:21:55 +0200

    Refactor class component to function

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-class InjectApp extends Component {
-  render() {
-    return (
-      <iframe
-            style={{
-              width: '100%',
-              height: '100%',
-            }}
-            frameBorder={0}
-            allowTransparency="true"
-            src={chrome.extension.getURL(`inject.html?protocol=${location.protocol}`)}
-          />
-    );
-  }
+export default function InjectApp(){
+  return(
+    <iframe
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          frameBorder={0}
+          allowTransparency="true"
+          src={chrome.extension.getURL(`inject.html?protocol=${location.protocol}`)}
+        />
+  );
 }
-
 window.addEventListener('load', () => {
   const injectDOM = document.createElement('div');
   injectDOM.className = 'inject-react-example';
