e351ecfb05a22fc1e4542ec453341ffb2929c15e
Michał Gołda <michalgolda@vp.pl>
2020-04-08 21:20:32 +0200

    Change require import to "from" and push this import to above storage loading

--- chrome/extension/spendlytime.js
+++ chrome/extension/spendlytime.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from '../../app/Root';
-
-import './spendlytime.css';
+import createStore from '../../app/store';
 
 chrome.storage.local.get('state', (obj) => {
     const { state } = obj;
     const initialState = state;
-
-    const createStore = require('../../app/store');
 
     ReactDOM.render(<Root store={createStore(initialState)} />, document.getElementById('root'));
   });
